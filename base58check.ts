69dfb465090796f257e840a8a42168e757390b42
Quentin Adam <quentinadam@gmail.com>
2024-12-26 02:31:25 +0100

    Add documentation. Bump version to v0.1.1.

--- base58check.ts
+++ base58check.ts
@@ -7,7 +7,36 @@ function computeChecksum(buffer: Uint8Array) {
   return sha256(sha256(buffer)).slice(0, 4);
 }
 
-export function decode(string: string, options?: base58.Options): Uint8Array {
+/** Options for {@linkcode encode} and {@linkcode decode}. */
+export interface Options {
+  /**
+   * The alphabet to use.
+   * The alphabet must be a string of 58 unique characters.
+   *
+   * @default {'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'}
+   */
+  alphabet?: string;
+}
+
+/**
+ * Encodes a Uint8Array buffer into a base58check string.
+ *
+ * @param buffer The buffer to encode.
+ * @param options The options to use for encoding.
+ * @returns The base58check encoded string.
+ */
+export function encode(buffer: Uint8Array, options?: Options): string {
+  return base58.encode(Uint8ArrayExtension.concat([buffer, computeChecksum(buffer)]), options);
+}
+
+/**
+ * Decodes a base58check encoded string into a Uint8Array buffer.
+ *
+ * @param string The base58check encoded string.
+ * @param options The options to use for decoding.
+ * @returns The decoded buffer.
+ */
+export function decode(string: string, options?: Options): Uint8Array {
   const { buffer, checkSum } = (() => {
     const buffer = base58.decode(string, options);
     assert(buffer.length >= 4);
@@ -15,8 +44,4 @@ export function decode(string: string, options?: base58.Options): Uint8Array {
   })();
   assert(new Uint8ArrayExtension(computeChecksum(buffer)).equals(checkSum));
   return buffer;
-}
-
-export function encode(buffer: Uint8Array, options?: base58.Options): string {
-  return base58.encode(Uint8ArrayExtension.concat([buffer, computeChecksum(buffer)]), options);
 }
